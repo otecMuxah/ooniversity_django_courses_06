af99b3e2d2514c246ab255363fda5a4b7d36df8e
otecMuxah <suxx_2000@hot,ail.com>
2017-03-28 00:43:31 +0300

    task6

--- courses/views.py
+++ courses/views.py
@@ -6,11 +6,13 @@ from django.shortcuts import render
 from . models import Course, Lesson
 
 def detail(request, course_id):
-    course_ = Course.objects.get(id=course_id)
-    lessons_list = Lesson.objects.filter(course=course_)
+    course = Course.objects.get(id=course_id)
+    lessons_list = Lesson.objects.filter(course=course)
+
 
     context = {
-        'course': course_[0],
+        'course': course,
         'lessons_list': lessons_list
+
     }
     return render(request, 'detail.html', context)
