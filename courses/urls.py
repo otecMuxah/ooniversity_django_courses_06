6965bf98eecdd379465cc3f48543b104ac42da6b
otecMuxah <suxx_2000@hot,ail.com>
2017-04-04 17:02:34 +0300

    task8 ver3

--- courses/urls.py
+++ courses/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^add/$', views.add, name='add'),
     url(r'^edit/(?P<course_id>\d+)/$', views.edit, name='edit'),
-    # url(r'^remove/(?P<course_id>\d+)/$', views.remove, name='remove'),
+    url(r'^remove/(?P<course_id>\d+)/$', views.remove, name='remove'),
     url(r'^(?P<course_id>[0-9]+)/add_lesson/$', views.add_lesson, name='add-lesson'),
 ]
