cd4163bfe2ca547e3b218c2ab5a228a8d69a9d4c
otecMuxah <suxx_2000@hot,ail.com>
2017-04-10 21:59:50 +0300

    feedbacks

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -7,7 +7,7 @@ class Feedback(models.Model):
     subject = models.CharField(max_length=200)
     message = models.TextField()
     from_email = models.EmailField()
-    create_date = models.DateField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} {}'.format(self.subject, self.name)
