4b3f45346750dcfee7f91d399f0a630e14c6acad
otecMuxah <suxx_2000@hot,ail.com>
2017-04-11 12:03:08 +0300

    quadratic3

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from django.core.exceptions import ValidationError
 
 
 class QuadraticForm(forms.Form):
@@ -11,7 +10,8 @@ class QuadraticForm(forms.Form):
     c = forms.IntegerField(label='коэффициент c')
 
     def clean_a(self):
-        a = self.cleaned_data['a']
-        if a == 0:
-            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        data = self.cleaned_data['a']
+        if data == 0:
+            raise forms.ValidationError('коэффициент при первом слагаемом уравнения не может быть равным нулю')
         return self.cleaned_data['a']
+
--- quadratic/urls.py
+++ quadratic/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from quadratic import views
 
 urlpatterns = [
-    url(r'^results/$', views.quadratic_results, name='results'),
+    url(r'results/$', views.quadratic_results, name='results'),
 ]
